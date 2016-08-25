491e0127bc5109290d38fc2129f35a802f36bf47
Ryan Vrba <ryan.vrba@gmail.com>
2016-08-25 15:10:13 -0700

    fixed small syntax errors

--- js/script.js
+++ js/script.js
@@ -19,12 +19,12 @@ function loadData() {
 
     $greeting.text( 'So, you want to live at ' + address +'?');
 
-    var streetviewURL = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address +'';
+    var streetviewURL = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $body.append('<img class="bgimg" src="' + streetviewURL +'">');
 
     // NY Times AJAX request
 
-    var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=9ea1aa776bff425da7a60de26822c242'
+    var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=9ea1aa776bff425da7a60de26822c242';
     $.getJSON(nytimesURL, function(data){
 
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
@@ -37,11 +37,11 @@ function loadData() {
                 '</a>'+
                 '<p>' + article.snippet + '</p>'+
             '</li>');
-        };
+        }
 
-    })
+    });
 
     return false;
-};
+}
 
 $('#form-container').submit(loadData);
